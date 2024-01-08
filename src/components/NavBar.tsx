9a87d0a7daab77b6c633e25c35314701c18cb159
mathias <mathiasdurat@gmail.com>
2024-01-08 15:28:06 +0100

    update vite version and implement redirect to lgin

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,9 @@
 import {Disclosure, Menu, Transition} from "@headlessui/react";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
-import {Fragment, useContext, useState} from "react";
-import {Link} from "react-router-dom";
+import {Fragment, useContext, useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
-import {AuthContext} from "../contexts/auth.tsx";
+import {AuthContext, defaultUser} from "../contexts/auth.tsx";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -25,7 +25,15 @@ const NavBar = () => {
   }]
   const [currentNav, setCurrentNav] = useState("Home")
   const {user, setUser} = useContext(AuthContext);
-
+  const navigate = useNavigate();
+  useEffect(() => {
+    init()
+  }, [""]);
+  const init = () => {
+    if (user === defaultUser) {
+      navigate("/login")
+    }
+  }
   function logout() {
     axios.get(
       import.meta.env.VITE_API_ENDPOINT + '/auth/me/logout',
@@ -111,58 +119,56 @@ const NavBar = () => {
 
   if (user.isAuth) {
     return (
-      <>
-        <Disclosure as="nav" className="bg-none backdrop-blur-sm absolute w-full">
-          {({open}) => (
-            <>
-              <div className="mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-between">
-                  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    {/* Mobile menu button*/}
-                    <Disclosure.Button
-                      className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                      <span className="absolute -inset-0.5"/>
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
-                      )}
-                    </Disclosure.Button>
+      <Disclosure as="nav" className="bg-none backdrop-blur-sm absolute w-full">
+        {({open}) => (
+          <>
+            <div className="mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button
+                    className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="absolute -inset-0.5"/>
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div
+                  className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
+                    <img
+                      className="h-8 w-auto"
+                      src="/logo.svg"
+                      alt="Your Company"/>
                   </div>
-                  <div
-                    className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex flex-shrink-0 items-center">
-                      <img
-                        className="h-8 w-auto"
-                        src="/logo.svg"
-                        alt="Your Company"/>
-                    </div>
-                    <div className="hidden sm:ml-6 sm:block">
-                      <div className="flex space-x-4">
-                        {navButton("computer")}
-                      </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {navButton("computer")}
                     </div>
                   </div>
-                  <div
-                    className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    {/* Profile dropdown */}
-                    {profileButton()}
-                  </div>
+                </div>
+                <div
+                  className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  {/* Profile dropdown */}
+                  {profileButton()}
                 </div>
               </div>
+            </div>
 
-              <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                  {navButton("mobile")}
-                </div>
-              </Disclosure.Panel>
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navButton("mobile")}
+              </div>
+            </Disclosure.Panel>
 
 
-            </>
-          )}
-        </Disclosure>
-      </>
+          </>
+        )}
+      </Disclosure>
     )
   }
 }
